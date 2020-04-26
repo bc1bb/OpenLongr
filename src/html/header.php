1787c8b5cf2933d0f5e2197715a8bbb40ac335db
Jus de Patate_ <jusdepatate@protonmail.com>
2020-04-26 13:30:08 +0200

    fix typo

--- src/html/header.php
+++ src/html/header.php
@@ -20,14 +20,14 @@ if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
     <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1">
     <meta name="application-name" content="<?= env('title') ?>">
     <meta name="msapplication-tooltip" content="<?= env('title') ?>"/>
-    <meta name="description" content="<?= env('title') ?> - Share messages easily & fastly.">
+    <meta name="description" content="<?= env('title') ?> - Share messages easily & quickly.">
 
     <meta property="og:url" content="<?= env('ext_url') ?>">
     <meta property="og:type" content="website">
     <meta property="og:title" content="<?= env('title') ?>">
     <meta property="og:image" content="<?= env('ext_url') ?>/src/img/logo.png">
-    <meta property="og:image:alt" content="<?= env('title') ?> - Share messages easily & fastly.">
-    <meta property="og:description" content="<?= env('title') ?> - Share messages easily & fastly.">
+    <meta property="og:image:alt" content="<?= env('title') ?> - Share messages easily & quickly.">
+    <meta property="og:description" content="<?= env('title') ?> - Share messages easily & quickly.">
     <meta property="og:site_name" content="<?= env('title') ?>">
     <meta property="og:locale" content="fr_FR">
 
@@ -35,8 +35,8 @@ if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
     <meta name="twitter:creator" content="@jusdepatate_">
     <meta name="twitter:url" content="<?= env('ext_url') ?>">
     <meta name="twitter:title" content="<?= env('title') ?>">
-    <meta name="twitter:description" content="<?= env('title') ?> - Share messages easily & fastly.">
-    <meta name="twitter:image:alt" content="<?= env('title') ?> - Share messages easily & fastly.">
+    <meta name="twitter:description" content="<?= env('title') ?> - Share messages easily & quickly.">
+    <meta name="twitter:image:alt" content="<?= env('title') ?> - Share messages easily & quickly.">
     <meta name="twitter:dnt" content="on">
 
     <title><?= env('title') ?></title>
