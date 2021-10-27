cfded41195293865731c6a3b28f56d546b043f38
Jus de Patate <jusdepatate@protonmail.com>
2021-10-27 19:16:09 +0200

    update status

--- status.php
+++ status.php
@@ -2,9 +2,8 @@
 header("Content-Type: application/json");
 require "autoload.php";
 
-$database = "OK";
 $status = "OK";
-// define them now so we dont have to define then later, they will be edited in case of error
+// define it now so we dont have to define it later, it'll be edited in case of error
 
 $dsn = "mysql:host=" . env("mysql_address") . ";dbname=" . env("mysql_database") . ";port=".env("mysql_port").";charset=utf8mb4";
 $options = [
@@ -16,15 +15,14 @@ try {
     $pdo = new PDO($dsn, env("mysql_username"), env("mysql_password"), $options); // if connection working
     $pdo->exec("use ". env("mysql_database"));
 } catch (\PDOException $e) {
-    $database = strtok($e, "\n"); // print only first line of error
+    $status = strtok($e, "\n"); // print only first line of error
 }
 
 try {
     $pdo->exec("describe LONGR"); // if table exist
 } catch (\PDOException $e) {
-    $database = strtok($e, "\n"); // print only first line of error
+    $status = strtok($e, "\n"); // print only first line of error
 }
 
-
 ?>
-{"status": "<?= $status ?>","database": "<?= $database ?>"}
+{"status": "<?= $status ?>"}
