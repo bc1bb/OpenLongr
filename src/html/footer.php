a3b4d8e95ff581c737868ebef523c912741ca50b
jusdepatate <jusdepatate@protonmail.com>
2022-04-19 21:21:39 +0200

    fix footer css + make btn white

--- src/html/footer.php
+++ src/html/footer.php
@@ -4,7 +4,7 @@ function add_footer(string $to_be_added = "") {
     ?>
     <footer>
         <?= $to_be_added."\n" ?>
-        <small class="grey"><span class="copyleft">&copy;</span> 2021 - Jus de Patate_ - <a href="https://github.com/jusdepatate/openlongr/commit/<?= get_current_git_commit() ?>"><?= get_current_git_commit("master", true) ?></a></small>
+        <small class="grey"><span class="copyleft">&copy;</span> 2022 - jusdepatate - <a href="https://github.com/jusdepatate/openlongr/commit/<?= get_current_git_commit() ?>"><?= get_current_git_commit("master", true) ?></a></small>
     </footer>
 </body>
 </html>
